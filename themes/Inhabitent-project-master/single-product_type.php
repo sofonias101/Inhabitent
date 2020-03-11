742b181d0aec0f814071590fc49f84742a53f538
sofonias101 <sofoniasasefaw@gmail.com>
2020-03-10 19:30:19 -0700

    single-product  page

--- themes/Inhabitent-project-master/single-product_type.php
+++ themes/Inhabitent-project-master/single-product_type.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php get_template_part( 'template-parts/content', ); ?>
 
 
 			
@@ -29,5 +29,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
